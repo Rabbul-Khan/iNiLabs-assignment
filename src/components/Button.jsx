92722a028882142ed3a63df10c85ba6ba58b1ec6
Rabbul-Khan <mohammed.rabbul.hossain.khan@g.bracu.ac.bd>
2024-03-05 22:01:02 +0600

    Update button animation ux

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,8 +5,8 @@ const Button = () => {
   return (
     <motion.button
       className="flex items-center justify-center gap-2 px-5 py-3 text-white transition duration-200 ease-in transform rounded-full bg-blue drop-shadow-sm hover:bg-blue-dark hover:drop-shadow-lg"
-      whileTap={{ scale: 0.95, transition: { duration: 0.5 } }}
-      whileHover={{ scale: 1.05, transition: { duration: 0.5 } }}
+      whileTap={{ scale: 0.95, transition: { duration: 0.025 } }}
+      whileHover={{ scale: 1.05, transition: { duration: 0.025 } }}
     >
       Sign up free <FaArrowRightLong className="mt-1 text-xs" />
     </motion.button>
